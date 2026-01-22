2fbe722f333947d913fd081da6b659acc6a24deb
Farmonbekfarxodov <farmonfarxodovbek@gmail.com>
2026-01-22 12:50:46 +0500

    .env fayliga ko'chirildi

--- anomaly_patterns_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnomalyPatternsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'anomaly_patterns_app'
--- anomaly_patterns_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "anomaly_patterns"
-
-urlpatterns = [
-    path('set-reference/',views.set_reference_segment,name='set_reference_segment'),
-]
